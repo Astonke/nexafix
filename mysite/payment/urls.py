a33c57faa1a350ef2905d4eae1b6fa1a0515bd43
Astonke <125920648+Astonke@users.noreply.github.com>
2024-10-27 10:02:09 +0300

    Update urls.py

--- mysite/payment/urls.py
+++ mysite/payment/urls.py
@@ -9,9 +9,5 @@ urlpatterns = [
     path('', default,name='default_page'),
     path('go-pay/', pay,name='payment_page'),
     path('submit_payment_proof/', submit_payment_proof, name='submit_payment_proof'),
-<<<<<<< HEAD
     path('vcode/<str:v_code>/', add_code,name='payment_page'),   
-=======
-
->>>>>>> 3f78c95d4af5d6e6d885eda22a52f553fa7973a7
 ]
