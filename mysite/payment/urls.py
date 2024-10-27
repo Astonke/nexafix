b263a37e6b445a5cb071ea2d09c74e3f3107366d
Astonke <125920648+Astonke@users.noreply.github.com>
2024-10-27 10:02:42 +0300

    Update urls.py

--- mysite/payment/urls.py
+++ mysite/payment/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path,include
-<<<<<<< HEAD
+
 from .views import default,pay,submit_payment_proof,add_code
-=======
+
 from .views import default,pay,submit_payment_proof
->>>>>>> 3f78c95d4af5d6e6d885eda22a52f553fa7973a7
 
 urlpatterns = [
     path('', default,name='default_page'),
